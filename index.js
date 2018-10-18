e1ea1e7cc2769fd5ccabf0d4ac59e674d3b2ff71
Bryan Wilson <cubefive@gmail.com>
2018-10-17 21:31:06 -0400

    tinkering

---
+++ index.js
@@ -0,0 +1,21 @@
+var calculator = {
+    add : function(number1, number2) {
+        return number1 + number2;
+    },
+    subtract : function(number1, number2) {
+        return number1 - number2;
+    },
+    multiply : function(number1, number2) {
+        return number1 * number2;
+    },
+    divide : function (number1, number2) {
+        return number1 / number2;
+    }
+};
+
+
+console.log(calculator.add(40, 100) 
+            + calculator.subtract(46, 227) 
+            + calculator.multiply(5, 20) 
+            + calculator.divide(90, 9))
+
