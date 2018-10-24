8579f905715940ef8fc29e65fc044112098311dc
Bryan Wilson <cubefive@gmail.com>
2018-10-24 07:55:49 -0400

    initial commit of for loops, js hw

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
-var calculator = {
-    add : function(number1, number2) {
-        return number1 + number2;
-    },
-    subtract : function(number1, number2) {
-        return number1 - number2;
-    },
-    multiply : function(number1, number2) {
-        return number1 * number2;
-    },
-    divide : function (number1, number2) {
-        return number1 / number2;
-    }
+const stuffObject = {
+    name: "Bryan",
+    age: 28,
+    gender: "Male",
+    coffee: true,
+    drink: "Beer",
+    id: 5
 };
 
-const stuff = "67"
+const stuffArray = [
+    "Bryan",
+    28,
+    "Male",
+    true,
+    "Beer",
+    5
+];
 
-console.log("wow")
+console.group("First:");
+for (let index = 0; index < stuffArray.length; index++) {
+    const element = stuffArray[index];
+    console.log(element)
+}
+console.groupEnd("END");
+
+console.group("Second:");
+stuffArray.forEach(element => {
+    console.log(element)
+});
+console.groupEnd("END");
+
+console.group("Third:");
+for (const key in stuffObject) {
+    console.log(key, stuffObject[key])
+}
+console.groupEnd("END");
+
+console.group("Fourth:");
+for (const iterator of stuffArray) {
+    console.log(iterator)
+}
+console.groupEnd("END");
 
 
-console.log(calculator.add(40, 100) 
-            + calculator.subtract(46, 227) 
-            + calculator.multiply(5, 20) 
-            + calculator.divide(90, 9))
+// // extra credit below
+// while (condition) {
 
-console.log("Testing bclean git alias")
+// }
+
+// do {
+
+// } while (condition);
+
